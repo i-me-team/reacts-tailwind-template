226a4e148e423b018a4b16d9b81c857cb60ee0ac
mandy8055_fe <msankrit@gmail.com>
2024-11-01 13:44:57 +0530

    feat: :tada: initial commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+// Problem link: https://devtools.tech/questions/s/how-to-build-a-confluence-like-sidebar-with-tree-structure-or-atlassian-frontend-interview-question-or-react-js---qid---3r7988VsESMQBt9vP1Zi
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
